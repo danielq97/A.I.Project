dea46a42ab289145384bd267bdf3efd55dae3494
JuanReyes3 <juan.reyes3@correo.icesi.edu.co>
2018-11-30 21:03:07 -0500

    atributos red

--- src/implementation/RedNeuronal.java
+++ src/implementation/RedNeuronal.java
@@ -1,9 +1,21 @@
 package implementation;
 
-public class RedNeuronal {
+import java.util.ArrayList;
 
-	public RedNeuronal(double[][] X, double[] Y, int[] capas) {
+public class RedNeuronal {
+	
+	private double[][] X;
+	private double[] Y;
+	private int[] tamCapasOcultas;
+	
+	private ArrayList<Capa> capas;
+	
+	public RedNeuronal(double[][] X, double[] Y, int[] tamCapasOcultas) {
+		this.X = X;
+		this.Y = Y;
+		this.tamCapasOcultas = tamCapasOcultas;
 		
+		this.capas = new ArrayList<Capa>();
 	}
 	
 	private void inicializarRed() {
